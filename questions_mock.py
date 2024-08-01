e83663911d327aa609c472adfae0fe9caeb86073
Tomás Lopes Oliveira <tomaslopes2711@gmail.com>
2024-08-01 17:56:53 +0100

    send questionary with a get request

---
+++ questions_mock.py
@@ -0,0 +1,6 @@
+
+questions = [
+    {"question": "What is your name?", "category": "personal"},
+    {"question": "How are you feeling today?", "category": "health"},
+    {"question": "What is your favorite color?", "category": "personal"},
+]
